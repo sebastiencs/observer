df2a1c40fba84578854439b0d43a1d27967bd6be
Sebastien Chapuis <sebastien@chapu.is>
2026-09-23 17:40:45 +0200

    Add ./githooks/pre-push

--- scripts/install-githooks.sh
+++ scripts/install-githooks.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 root="$(git rev-parse --show-toplevel)"
 cd "$root"
 
-chmod +x .githooks/pre-commit
+chmod +x .githooks/pre-commit .githooks/pre-push
 git config core.hooksPath .githooks
 
 echo "Git hooks enabled: core.hooksPath=.githooks"
